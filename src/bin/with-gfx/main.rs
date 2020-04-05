3e14c7a730d5296a084bade968cfd876bb379e0f
Shub1427 <shub1493biswas@gmail.com>
2020-04-05 16:13:59 +0530

    Day 029: add collision detection as a aseparate function

--- src/bin/with-gfx/main.rs
+++ src/bin/with-gfx/main.rs
@@ -10,26 +10,28 @@ fn run(window: Window, event_loop: EventLoop<()>) {
         println!("{:?}", event);
 
         match event {
-            Event::WindowEvent { event, window_id, .. } => match event {
-                WindowEvent::CloseRequested if window_id == window.id() => *control_flow = ControlFlow::Exit,
+            Event::WindowEvent {
+                event, window_id, ..
+            } => match event {
+                WindowEvent::CloseRequested if window_id == window.id() => {
+                    *control_flow = ControlFlow::Exit
+                }
                 WindowEvent::MouseInput {
                     state: ElementState::Released,
                     ..
                 } => {
                     println!(">>>>>>>>>>>>>>>> Window will Redraw <<<<<<<<<<<<<<<<");
                     window.request_redraw();
-                },
-                WindowEvent::Resized {..} => {
+                }
+                WindowEvent::Resized { .. } => {
                     println!(">>>>>>>>>>>>>>> Resized <<<<<<<<<<<<<<");
-                },
+                }
                 _ => (),
             },
             Event::MainEventsCleared => {
                 window.request_redraw();
-            },
-            Event::RedrawRequested(window_id) => {
-
-            },
+            }
+            Event::RedrawRequested(window_id) => {}
             _ => (),
         }
     });
--- src/bin/with-ggez/main.rs
+++ src/bin/with-ggez/main.rs
@@ -1,16 +1,22 @@
 // Basic GGez requires EventHandler, Context and Graphics.
 use ggez::event;
 use ggez::graphics;
-use ggez::{ Context, ContextBuilder, GameResult };
+use ggez::{Context, ContextBuilder, GameResult};
 // Some extra imports
 use ggez::conf;
-use ggez::timer;
 use ggez::input::keyboard;
-use ggez::nalgebra::{Point2};
+use ggez::nalgebra::Point2;
+use ggez::timer;
 
 // Static vars
+const TILE_OFFSET_X: f32 = -40.0;
+const TILE_OFFSET_Y: f32 = 0.0;
+const TILE_COUNT_X: u8 = 17;
+const TILE_COUNT_Y: u8 = 9;
 const TILE_WIDTH: u8 = 80;
 const TILE_HEIGHT: u8 = 80;
+const PLAYER_WIDTH: u8 = 40;
+const PLAYER_HEIGHT: u8 = 40;
 const FPS: u32 = 60;
 
 struct GameState {
@@ -25,41 +31,91 @@ struct GameState {
 impl GameState {
     fn new(ctx: &mut Context) -> GameResult<Self> {
         let tile_map = vec![
-            vec![0,0,0,0, 0,1,0,0, 0,0,0,0, 0,0,0,0, 0],
-            vec![0,0,0,0, 1,1,1,0, 0,0,0,1, 1,1,0,0, 0],
-            vec![0,0,1,1, 1,0,1,0, 0,0,0,1, 0,1,0,0, 0],
-            vec![0,0,1,0, 1,1,1,1, 1,0,0,1, 1,1,1,1, 0],
-            vec![0,1,1,1, 1,1,1,1, 1,1,0,1, 1,1,1,1, 1],
-            vec![0,0,1,1, 0,1,0,0, 0,1,1,1, 1,1,1,1, 0],
-            vec![0,0,0,1, 1,1,1,1, 0,1,1,1, 0,1,0,0, 0],
-            vec![0,0,0,0, 0,0,0,1, 1,1,1,1, 1,1,0,0, 0],
-            vec![0,0,0,0, 0,0,0,1, 0,0,0,0, 0,0,0,0, 0],
+            vec![0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            vec![0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+            vec![0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0],
+            vec![0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0],
+            vec![0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1],
+            vec![0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0],
+            vec![0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0],
+            vec![0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+            vec![0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         ];
         let player = graphics::Mesh::new_rectangle(
             ctx,
             graphics::DrawMode::fill(),
-            graphics::Rect::new(
-                0f32, 0f32, TILE_WIDTH as f32 / 2.0, TILE_HEIGHT as f32 / 2.0
-            ),
-            graphics::Color::new(0.9, 0.56, 0.9, 1.0)
+            graphics::Rect::new(0f32, 0f32, PLAYER_WIDTH as f32, PLAYER_HEIGHT as f32),
+            graphics::Color::new(0.9, 0.56, 0.9, 1.0),
         )?;
         Ok(GameState {
             bg_color: graphics::Color::new(0.878, 0.878, 0.878, 1.0),
-            tile_map_start_x: -40.0, // Will move half tile left, so that more tiles are visible
-            tile_map_start_y: 0.0,
+            tile_map_start_x: TILE_OFFSET_X, // Will move half tile left, so that more tiles are visible
+            tile_map_start_y: TILE_OFFSET_Y,
             tile_map,
             player,
-            player_pos: Point2::new(2.0 * TILE_WIDTH as f32, 4.0 * TILE_HEIGHT as f32)
+            player_pos: Point2::new(2.0 * TILE_WIDTH as f32, 4.0 * TILE_HEIGHT as f32),
         })
     }
+}
+
+fn is_valid_location(state: &GameState, pos_x: &f32, pos_y: &f32) -> bool {
+    let player_tile_x_start = (pos_x / TILE_WIDTH as f32).floor();
+    let player_tile_x_end = ((pos_x + PLAYER_WIDTH as f32) / TILE_WIDTH as f32).floor();
+    let player_tile_y_start = (pos_y / TILE_HEIGHT as f32).floor();
+    let player_tile_y_end = ((pos_y + PLAYER_HEIGHT as f32) / TILE_HEIGHT as f32).floor();
+
+    if player_tile_x_start >= 0.0
+        && player_tile_x_end < TILE_COUNT_X as f32
+        && player_tile_y_start >= 0.0
+        && player_tile_y_end < TILE_COUNT_Y as f32
+    {
+        let vertex_top_left =
+            state.tile_map[player_tile_y_start as usize][player_tile_x_start as usize];
+        let vertex_top_right =
+            state.tile_map[player_tile_y_start as usize][player_tile_x_end as usize];
+        let vertex_bottom_right =
+            state.tile_map[player_tile_y_end as usize][player_tile_x_start as usize];
+        let vertex_bottom_left =
+            state.tile_map[player_tile_y_end as usize][player_tile_x_end as usize];
+
+        return vertex_top_left == 1
+            && vertex_top_right == 1
+            && vertex_bottom_right == 1
+            && vertex_bottom_left == 1;
+    }
+
+    return false;
 }
 
 // We need our state to have implemented the EventHandler Trait
 impl event::EventHandler for GameState {
     fn update(&mut self, ctx: &mut Context) -> GameResult {
-        // Do Nothing for now
         while timer::check_update_time(ctx, FPS) {
+            let old_pos_x = self.player_pos.x;
+            let old_pos_y = self.player_pos.y;
+            let mut del_x: f32 = 0.0;
+            let mut del_y: f32 = 0.0;
+            // These can be considered as Level-Triggered Key Presses
+            if keyboard::is_key_pressed(ctx, event::KeyCode::Up) {
+                del_y = -1.0;
+            }
+            if keyboard::is_key_pressed(ctx, event::KeyCode::Right) {
+                del_x = 1.0;
+            }
+            if keyboard::is_key_pressed(ctx, event::KeyCode::Down) {
+                del_y = 1.0;
+            }
+            if keyboard::is_key_pressed(ctx, event::KeyCode::Left) {
+                del_x = -1.0;
+            }
 
+            let new_pos_x = old_pos_x + del_x;
+            let new_pos_y = old_pos_y + del_y;
+
+            if is_valid_location(self, &new_pos_x, &new_pos_y) {
+                self.player_pos.x = new_pos_x;
+                self.player_pos.y = new_pos_y;
+            }
         }
         Ok(())
     }
@@ -72,22 +128,34 @@ impl event::EventHandler for GameState {
         for (row, row_ele) in self.tile_map.iter_mut().enumerate() {
             for (col, element) in row_ele.iter().enumerate() {
                 let mut color = graphics::Color::new(1.0, 1.0, 1.0, 1.0);
-                if *element == 1 { color = graphics::Color::new(0.5, 0.5, 0.5, 1.0); }
+                if *element == 1 {
+                    color = graphics::Color::new(0.5, 0.5, 0.5, 1.0);
+                }
                 let rect = graphics::Rect::new(
                     col as f32 * TILE_WIDTH as f32,
                     row as f32 * TILE_HEIGHT as f32,
                     TILE_WIDTH as f32,
-                    TILE_HEIGHT as f32
+                    TILE_HEIGHT as f32,
                 );
                 let r1 =
                     graphics::Mesh::new_rectangle(ctx, graphics::DrawMode::fill(), rect, color)?;
-                graphics::draw(ctx, &r1, graphics::DrawParam::default()
-                    .dest(Point2::new(self.tile_map_start_x, self.tile_map_start_y))
+                graphics::draw(
+                    ctx,
+                    &r1,
+                    graphics::DrawParam::default()
+                        .dest(Point2::new(self.tile_map_start_x, self.tile_map_start_y)),
                 )?;
             }
         }
 
-        graphics::draw(ctx, &self.player, graphics::DrawParam::default().dest(self.player_pos))?;
+        graphics::draw(
+            ctx,
+            &self.player,
+            graphics::DrawParam::default().dest(Point2::new(
+                self.player_pos.x + TILE_OFFSET_X,
+                self.player_pos.y + TILE_OFFSET_Y,
+            )),
+        )?;
 
         graphics::present(ctx)?; // Show the Background
         timer::yield_now();
@@ -96,46 +164,15 @@ impl event::EventHandler for GameState {
 
     // ggez keyboard events are edge-triggered, thus we need to check
     // every key event to verify if multiple keys are pressed and make the changes accordingly.
+    // These are Edge-Triggered Key Presses
     fn key_down_event(
         &mut self,
         ctx: &mut Context,
         keycode: event::KeyCode,
         _keymod: event::KeyMods,
-        _repeat: bool
+        _repeat: bool,
     ) {
         match keycode {
-            event::KeyCode::Up => {
-                self.player_pos.y -= 8.0;
-                if keyboard::is_key_pressed(ctx, event::KeyCode::Left) {
-                    self.player_pos.x -= 8.0;
-                } else if keyboard::is_key_pressed(ctx, event::KeyCode::Left) {
-                    self.player_pos.x += 8.0;
-                }
-            }
-            event::KeyCode::Left => {
-                self.player_pos.x -= 8.0;
-                if keyboard::is_key_pressed(ctx, event::KeyCode::Up) {
-                    self.player_pos.y -= 8.0;
-                } else if keyboard::is_key_pressed(ctx, event::KeyCode::Down) {
-                    self.player_pos.y += 8.0;
-                }
-            }
-            event::KeyCode::Right => {
-                self.player_pos.x += 8.0;
-                if keyboard::is_key_pressed(ctx, event::KeyCode::Up) {
-                    self.player_pos.y -= 8.0;
-                } else if keyboard::is_key_pressed(ctx, event::KeyCode::Down) {
-                    self.player_pos.y += 8.0;
-                }
-            }
-            event::KeyCode::Down => {
-                self.player_pos.y += 8.0;
-                if keyboard::is_key_pressed(ctx, event::KeyCode::Left) {
-                    self.player_pos.x -= 8.0;
-                } else if keyboard::is_key_pressed(ctx, event::KeyCode::Left) {
-                    self.player_pos.x += 8.0;
-                }
-            }
             event::KeyCode::Escape => event::quit(ctx),
             _ => (), // Do nothing
         }
@@ -151,7 +188,8 @@ fn main() -> GameResult {
     // Get GGez Contert and Window Event Loop;
     let (ctx, event_loop) = &mut ContextBuilder::new("Handmade Hero", "Subroto")
         .conf(window_config)
-        .build().unwrap();
+        .build()
+        .unwrap();
 
     // Instantiate Game State:
     let mut initial_state = GameState::new(ctx)?;
